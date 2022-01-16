87b1ba6654851276eac6b74ff7fd88603d9c4505
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2022-01-16 08:59:39 +0200

    Use a lookup table instead of a switch in disassembler

--- new/VM/src/disassembler.c
+++ new/VM/src/disassembler.c
@@ -2,47 +2,19 @@
 #include "common.h"
 #include "disassembler.h"
 
+static const char *instructions[] = {
+    "PUSH", "POP", "POPTO",
+    "MOV", "LD", "ST",
+    "ADD", "SUB", "MUL", "DIV",
+    "JMP", "JEQ", "JNE", "JGT", "JLT",
+    "REQ", "NOP", "HLT"
+};
+
 const char *instr2str(OpCode instruction) {
-    switch(instruction) {
-        case OP_PUSH:
-            return "PUSH";
-        case OP_POP:
-            return "POP";
-        case OP_POPTO:
-            return "POPTO";
-        case OP_MOV:
-            return "MOV";
-        case OP_LD:
-            return "LD";
-        case OP_ST:
-            return "ST";
-        case OP_ADD:
-            return "ADD";
-        case OP_SUB:
-            return "SUB";
-        case OP_MUL:
-            return "MUL";
-        case OP_DIV:
-            return "DIV";
-        case OP_JMP:
-            return "JMP";
-        case OP_JEQ:
-            return "JEQ";
-        case OP_JNE:
-            return "JNE";
-        case OP_JGT:
-            return "JGT";
-        case OP_JLT:
-            return "JLT";
-        case OP_REQ:
-            return "REQ";
-        case OP_NOP:
-            return "NOP";
-        case OP_HLT:
-            return "HLT";
-        default:
-            LOGERR("Unknown instruction 0x%X!", instruction);
-            break;
+    if(Instruction < INSTRUCTION_COUNT) {
+        return instructions[instruction];
+    } else {
+        LOGERR("Unknown instruction 0x%X!", instruction);
+        return "UNKNOWN";
     }
-    return "UNKNOWN";
 }
